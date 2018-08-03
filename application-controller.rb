3dc33649fdf2646ab265ac89bf5963c009692d3b
EC2 Default User <ec2-user@ip-172-31-1-187.us-east-2.compute.internal>
2018-08-03 14:43:12 +0000

    First Commit

--- application-controller.rb
+++ application-controller.rb
@@ -7,5 +7,65 @@ class MyApp < Sinatra::Base
  get'/' do
   erb :index
  end
+ 
+ get '/questions.erb' do
+  erb :questions
+ end
+ 
+ get '/great.erb' do
+  erb :great
+ end
+ 
+ get '/good.erb' do
+  erb :good
+ end
+ 
+ get '/okay.erb' do
+  erb :okay
+end
 
+get '/iffy.erb' do
+ erb :iffy
+end
+
+get 'bad.erb' do
+ erb :bad
+end
+
+get '/terrible.erb' do
+ erb :terrible
+end
+
+get '/getHelp.erb' do
+ erb :getHelp
+end
+
+get '/websites.erb' do
+ erb :websites
+end
+
+ post  '/results' do
+   answers = params.values
+   @total= 0
+   answers.each do |answer|
+     @total += answer.to_i
+   end
+   puts @total
+   
+ @health = mental_state(@total)
+   if @health == "great"
+     erb :great
+   elsif @health == "good"
+     erb :good
+   elsif @health == "okay"
+     erb :okay
+   elsif @health == "iffy"
+     erb :iffy
+   elsif @health == "bad"
+     erb :bad
+   elsif @health == "terrible"
+     erb :terrible
+   end
+    
+  end
 end
--- models/questions.rb
+++ models/questions.rb
@@ -1,1 +1,16 @@
 # This is where your Ruby code goes
+def mental_state(user_value)
+ if user_value == 0
+    "great"
+ elsif user_value == 1
+    "good"
+ elsif user_value == 2
+    "okay"
+ elsif user_value == 3
+    "iffy"
+ elsif user_value == 4
+    "bad"
+ elsif user_value == 5
+    "terrible"
+ end
+end
